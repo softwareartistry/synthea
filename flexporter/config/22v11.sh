c0fabb82cccd14a43210c165c64c91c639204b8f
D.Sharath <112467530+Sharath314e@users.noreply.github.com>
2024-07-02 11:22:03 +0530

    Update 22v11.sh

--- flexporter/config/22v11.sh
+++ flexporter/config/22v11.sh
@@ -14,4 +14,3 @@
   ./run_synthea -p 125 -a 19-85 -r 20230101 -fm /Users/d.sharath/Documents/Personal/PyCharmProjects/syntheadataconfig/app/syntheadataconfig/22/v11/numerator_4.yml --exporter.baseDirectory="flexporter/output/22/v11/";
   ./run_synthea -p 125 -a 19-85 -r 20230101 -fm /Users/d.sharath/Documents/Personal/PyCharmProjects/syntheadataconfig/app/syntheadataconfig/22/v11/numerator_5.yml --exporter.baseDirectory="flexporter/output/22/v11/";
 } > flexporter/log/22v11.log
-scp -P 54196 -r /Users/d.sharath/Documents/Personal/IdeaProjects/synthea/flexporter/output/22 root@127.0.0.1:/data/flexporter/output/
